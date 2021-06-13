6e481db75c847f9fd1e2c95930e4b6b970f10d63
Md Suhaib Safwan <suhaibsafwan45@gmail.com>
2021-06-14 01:37:40 +0600

    Updated Again...

--- GossipBooks/settings.py
+++ GossipBooks/settings.py
@@ -239,7 +239,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_dir')
 
 STATIC_URL = '/static/'
 
