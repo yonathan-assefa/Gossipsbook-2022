dafccc62cced62584d7d1259f041f1de53877f37
Md Suhaib Safwan <suhaibsafwan45@gmail.com>
2021-06-23 12:31:26 +0600

    Fixed The Error that was in gossips-page...

--- controls/urls.py
+++ controls/urls.py
@@ -5,9 +5,9 @@ app_name = 'controls'
 
 urlpatterns = [
     path('welcome', welcome, name='welcome'),
-    path('rfr', rfr, name='rfr'),
-    path('feedback', feedback, name='feedback'),
-    path('privacy_policy', privacy_policy, name='privacy_policy'),
-    path('false_information', false_information, name='false_information'),
+    # path('rfr', rfr, name='rfr'),
+    # path('feedback', feedback, name='feedback'),
+    # path('privacy_policy', privacy_policy, name='privacy_policy'),
+    # path('false_information', false_information, name='false_information'),
     path('', index, name='index_page'),
 ]
