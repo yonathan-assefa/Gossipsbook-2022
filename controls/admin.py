ec46ffcbc7f048716235c8889a3d29a826879877
Md Suhaib Safwan <suhaibsafwan45@gmail.com>
2021-06-25 15:36:18 +0600

    Modified The DBS for controls...

--- controls/admin.py
+++ controls/admin.py
@@ -2,17 +2,6 @@ from django.contrib import admin
 from .models import FalseInformation, RFRModel, FeedbackModel
 
 # Register your models here.
-
-@admin.register(FalseInformation)
-class FalseInformationAdmin(admin.ModelAdmin):
-    list_display = ['gossip', 'cheater']
-
-
-@admin.register(RFRModel)
-class RFRAdmin(admin.ModelAdmin):
-    list_display = ['post_id', 'post_title', 'reason', 'section']
-
-
-@admin.register(FeedbackModel)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ['user', 'email', 'message']
+admin.register(FalseInformation)
+admin.register(RFRModel)
+admin.register(FeedbackModel)
--- controls/models.py
+++ controls/models.py
@@ -1,20 +1,20 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.db import models
-
-from gossips.models import GossipsModel
+from django.http import Http404
+from gossips.models import GossipsModel, random_number_gen
 from cheaters.models import CheatersModel
+from django.utils.text import slugify
 
 
 class FalseInformation(models.Model):
    gossip = models.ForeignKey(GossipsModel, on_delete=models.CASCADE, related_name='false_gossip', null=True, blank=True)
-   cheater = models.ForeignKey(CheatersModel, on_delete=models.CASCADE, related_name='false_cheater', null=True, blank=True)
    section = models.CharField(max_length=255)
    date_published = models.DateTimeField(auto_now_add=True, verbose_name='Date Published')
    date_updated = models.DateTimeField(auto_now=True, verbose_name='Date Updated')
 
    def __str__(self):
-      return f"{self.gossip if self.gossip else self.cheater}"
+      return f"{self.gossip}"
     
 
 # signal that calculates whether a gossip should be added to false information section or not
@@ -54,21 +54,36 @@ post_save.connect(cheater_false_information, sender=CheatersModel)
 
 class RFRModel(models.Model):
    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-   reason = models.CharField(max_length=955)
+   gossip = models.ForeignKey(GossipsModel, on_delete=models.SET_NULL, null=True)
+   reason = models.TextField(max_length=955)
    section = models.CharField(max_length=255)
-   post_id = models.CharField(max_length=9255)
-   post_title = models.CharField(max_length=255)
    date_submitted = models.DateTimeField(auto_now_add=True)
 
    def __str__(self):
       return self.reason
 
+   class Meta:
+      unique_together = ["user", "gossip"]
+
 
 class FeedbackModel(models.Model):
-   user = models.CharField(max_length=255) # because non logged in users can give feedback
-   email = models.CharField(max_length=255) # because non logged in users can give feedback
+   user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # because non logged in users can give feedback
+   email = models.EmailField(null=True) # because non logged in users can give feedback
+   slug = models.SlugField()
    message = models.CharField(max_length=955)
    date_submitted = models.DateTimeField(auto_now_add=True)
 
    def __str__(self):
       return self.message
+
+   def save(self, *args, **kwargs):
+      user = self.user
+      email = self.email
+      if (not user) and (not email):
+         raise Http404("Please Provide an Email or be Authenticated...")
+
+      slug = self.slug
+      if (slug is None) or (slug == ""):
+         self.slug = slugify(self.message[:10] + random_number_gen(9))
+
+      return super().save(*args, **kwargs)
